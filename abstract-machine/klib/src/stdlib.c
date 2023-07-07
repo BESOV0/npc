3204aa9577582a8d4d2f61ef1d1a80efc4298ab6
ysyx_22050598-Xing Gen <xinggen051@163.com>
2023-07-07 15:17:05 +0800

    NJU-ProjectN/nanos-lite ics2021 initialized
    NJU-ProjectN/nanos-lite 2a141760e31be246a7316942293a97873925bc2f Makefile: use header files in newlib-navy

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -28,15 +28,23 @@ int atoi(const char* nptr) {
   }
   return x;
 }
-
+static char* addr = NULL;
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+  if(!addr) addr = (char*)ROUNDUP(heap.start, 8);
+  char* ret = addr;
+  size = ROUNDUP(size, 8);
+  addr += size;
+  return ret;
+  /*
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
   panic("Not implemented");
 #endif
   return NULL;
+  */
+
 }
 
 void free(void *ptr) {
