fdde27c666f6f8c96d22db01aad069ec0f4d8a15
BESOV0 <115919447+BESOV0@users.noreply.github.com>
2023-10-19 23:43:46 +0800

    Delete navy-apps/tests/timer-test directory

--- navy-apps/tests/timer-test/build/native/main.d
+++
@@ -1,3 +0,0 @@
-/home/xg/ysyx-workbench/navy-apps/tests/timer-test/build/native/main.o: \
- /home/xg/ysyx-workbench/navy-apps/tests/timer-test/main.c \
- /home/xg/ysyx-workbench/navy-apps/libs/libndl/include/NDL.h
